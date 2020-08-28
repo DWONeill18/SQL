2e094f8035c670535eea7db6ca5d6e12ef3c008f
DWONeill18 <d.w.oneill.18@gmail.com>
2020-08-28 20:47:38 +0100

    Added temporary table months needed for churn calculation

--- churn_projects/Codeflix_churn/churn_rates.sql
+++ churn_projects/Codeflix_churn/churn_rates.sql
@@ -23,3 +23,21 @@ Determine the range of months of data provided. Which months will you be able to
 
 SELECT min(subscription_start), max(subscription_end)
 FROM subscriptions;
+
+/* You’ll be calculating the churn rate for both segments (87 and 30) over the first 3 months of 2017 (you can’t calculate it for December,
+ since there are no subscription_end values yet). To get started, create a temporary table of months. */
+
+WITH months AS
+(SELECT 
+  '2017-01-01' AS first_day,
+  '2017-01-31' AS last_day
+UNION
+SELECT 
+  '2017-02-01' AS first_day,
+  '2017-02-28' AS last_day
+UNION
+SELECT
+  '2017-03-01' AS first_day,
+  '2017-03-31' AS last_day
+)
+
