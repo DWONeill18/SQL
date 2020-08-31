1a16987d7e026b7bfe3306e65864bc6141a81638
DWONeill18 <d.w.oneill.18@gmail.com>
2020-08-31 23:31:39 +0100

    Added queries for to understand what the user journey was

--- marketing_attribution/marketing_attribution.sql
+++ marketing_attribution/marketing_attribution.sql
@@ -31,3 +31,53 @@ ORDER BY utm_source;
 SELECT DISTINCT page_name
 FROM page_visits
 ORDER BY page_name;
+
+/* What is the user journey? */
+
+/* How many first touches is each campaign responsible for? */
+
+WITH first_touch AS (
+    SELECT user_id,
+        MIN(timestamp) as first_touch_at
+    FROM page_visits
+    GROUP BY user_id),
+ft_attr AS (
+  SELECT ft.user_id,
+      ft.first_touch_at,
+      pv.utm_source,
+		  pv.utm_campaign
+  FROM first_touch ft
+  JOIN page_visits pv
+     ON ft.user_id = pv.user_id
+     AND ft.first_touch_at = pv.timestamp
+)
+SELECT ft_attr.utm_source,
+       ft_attr.utm_campaign,
+       COUNT(*)
+FROM ft_attr
+GROUP BY 2
+ORDER BY 3 DESC;
+
+/* How many last touches is each campaign ressponsbile for? */
+
+WITH last_touch AS (
+    SELECT user_id,
+        MAX(timestamp) as last_touch_at
+    FROM page_visits
+    GROUP BY user_id),
+lt_attr AS (
+  SELECT lt.user_id,
+      lt.last_touch_at,
+      pv.utm_source,
+		  pv.utm_campaign
+  FROM last_touch lt
+  JOIN page_visits pv
+     ON lt.user_id = pv.user_id
+     AND lt.last_touch_at = pv.timestamp
+)
+SELECT lt_attr.utm_source, 
+       lt_attr.utm_campaign,
+       COUNT(*)
+FROM lt_attr
+GROUP BY 2
+ORDER BY 3 DESC;
