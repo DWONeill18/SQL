830d99bbebe2ba69577d3cf44855d5e638536498
DWONeill18 <d.w.oneill.18@gmail.com>
2020-09-01 14:39:27 +0100

    Added queries to determine how many visitors make a purchase and which campaigns produced the msot unique users to the purchase page

--- marketing_attribution/marketing_attribution.sql
+++ marketing_attribution/marketing_attribution.sql
@@ -81,3 +81,40 @@ SELECT lt_attr.utm_source,
 FROM lt_attr
 GROUP BY 2
 ORDER BY 3 DESC;
+
+/* How many visitors make a purchase? */
+
+SELECT COUNT(DISTINCT user_id)
+FROM page_visits
+WHERE page_name = '4 - purchase';
+
+/* How many last touches on the purchase page is each campaign responsible for? */
+
+WITH last_touch AS (
+    SELECT user_id,
+        MAX(timestamp) as last_touch_at
+    FROM page_visits
+    WHERE page_name = '4 - purchase' ----- WHERE clause on the last touch temp table
+    GROUP BY user_id),
+lt_attr AS (
+  SELECT lt.user_id,
+      lt.last_touch_at,
+      pv.utm_source,
+		  pv.utm_campaign
+  FROM last_touch lt
+  JOIN page_visits pv
+     ON lt.user_id = pv.user_id
+     AND lt.last_touch_at = pv.timestamp
+)
+SELECT lt_attr.utm_source, 
+       lt_attr.utm_campaign,
+       COUNT(*)
+FROM lt_attr
+GROUP BY 2
+ORDER BY 3 DESC;
+
+/* CoolTShirts can re-invest in 5 campaigns. Given your findings in the project, which should they pick and why? */
+
+/* By ordering and limiting to 5 rows the last query we can see which 5 campaigns produced the most last-touch purchase page hits.
+This is ideal cause the user is more likely to purchase something if they hit his page, so campaigns that get them there are the
+most profitable. */
